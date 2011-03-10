e15c775f06961e5e6977d7f463da31c8dced69a4
Ben Zittlau <ben@zittlau.ca>
2011-03-09 19:42:11 -0700

    First commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+Spike-Base
+=======
+The purpose of this repo is to create a base rails project configured as I typically setup my projects to be used for easy experiments.  The intention is for this project to evolve along with my style and incorporate the tools (e.g. jquery, rspec, etc.) that I have adopted.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spike::Application.config.secret_token = '2f96c03c5ec413ff96e2101dceb32ad27571469441e50b5811d304bb12c0f857a883527e32d2e786b70f4d9342ca50ee7ae18b50fe18631e2c0be3315324b789'
