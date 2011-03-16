d39cd0ff2a642c64a83fbfccdbf92d6dd9ce6017
Ben Zittlau <ben@zittlau.ca>
2011-03-15 18:55:06 -0600

    Fixed and added some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,22 @@ describe User do
   it "should be valid" do
     User.new.should be_valid
   end
+  
+  describe "should have the right attributes" do
+    before(:each) do
+      @user = User.new
+    end
+  
+    it "should response to name" do
+      @user.should respond_to(:name)
+    end
+  
+    it "should respond to email" do
+      @user.should respond_to(:email)
+    end
+  
+    it "should respond to phone_number" do
+      @user.should respond_to(:phone_number)
+    end
+  end
 end
