298df12ef4144a2ca9b07ebec86f0034561c3516
Ben Zittlau <ben@zittlau.ca>
2011-03-22 23:04:24 -0600

    Added more intelligent action text for reverting

--- app/models/user_version.rb
+++ app/models/user_version.rb
@@ -1,4 +1,8 @@
 class UserVersion < VestalVersions::Version
   #idea, make the set_table_name polymorphic, have this one class for all models
   set_table_name :user_versions
+  
+  def reverted?
+    self.reverted_from != nil
+  end
 end
